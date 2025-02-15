3cbd547eff4ada549613f7b5403057b8b6f99747
AngelVillalobosS <angel.villalobos2269@uppuebla.edu.mx>
2025-02-15 12:40:57 -0600

    v3

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const alumno = <div>Hola</div>
+const alumnoTic={
+    nombre:'Natalia',
+    apodo:'naty',
+    salon: '101'
+}
+
+function informacionAlumno(alumnoTic) {
+    return `Alumno : ${alumnoTic.nombre} de apodo: ${alumnoTic.apodo}`
+}
+
+function salon(alumnoTic){
+  return `${alumnoTic.salon}`
+}
+
+const elemento = <div>Hola {informacionAlumno(alumnoTic)} del salon {salon(alumnoTic)}</div>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  alumno
+  elemento
 );
 
